f857f71ec4b4733973df407dd7b36407565edb77
Philipe Rodrigues <dev.philipe@outlook.com>
2023-12-07 13:39:34 -0300

    build(i18n): configures application to start translation

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "welcome#index"
+  root to: redirect("/#{I18n.default_locale}", status: 302), as: :redirected_root
+
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    root to: "welcome#index", as: :welcome
+  end
 end
