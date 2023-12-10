8b4243d0395a26c835fae27aed4e891a4d0b3eaa
Philipe Rodrigues <dev.philipe@outlook.com>
2023-12-10 19:46:55 -0300

    feat(application index): enables user to send message

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,22 @@
 class WelcomeController < ApplicationController
   def index
+    @message = Message.new
+  end
+
+  def create_message
+    @message = Message.new(message_params)
+    if @message.save
+      flash[:success] = 'Mensagem enviada com sucesso!'
+      redirect_to welcome_path
+    else
+      flash[:error] = 'Houve um erro ao tentar enviar a mensagem.'
+      redirect_to welcome_path
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:person_name, :person_mail, :description)
   end
 end
