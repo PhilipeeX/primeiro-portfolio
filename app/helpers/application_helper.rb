55bcd367d29d1d0d50cbb80873a40faa8690d88c
Philipe Rodrigues <dev.philipe@outlook.com>
2023-12-10 15:44:39 -0300

    refactor(application view): creates helper to return correct button translation link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def translation_link
+    link_locale = I18n.locale == :en ? 'pt-BR' : 'en'
+    image_path = I18n.locale == :en ? 'application/brazil-flag-button.png' : 'application/usa-flag-button.png'
+
+    link_to(url_for(locale: link_locale)) do
+      image_tag(image_path, class: 'translate-button')
+    end
+  end
 end
